ec2239d66e686281f0c31db81f203c45b9c800d1
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-08-03 08:11:46 +0300

    Cleanup
     * "codingGround" -> "codeground"
     * Remove dependy for Base64
     * "companyEmail" -> "email"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,3 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 (function(document) {
   'use strict';
 
@@ -23,15 +14,14 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
   app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
+    console.log('Caching complete.');
   };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
     var challengeKey = localStorage.getItem("challenge");
-    if(challengeKey!="" && challengeKey!= undefined){
+    if (challengeKey != "" && challengeKey != undefined) {
       app.requestData(challengeKey)
     }
   });
@@ -50,7 +40,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
   app.requestData = function(challengeKey){
-    if(challengeKey!="" && challengeKey!= undefined){
+    if (challengeKey!="" && challengeKey!= undefined) {
       localStorage.setItem("challenge", challengeKey);
       post(app.$.resultRequest, "/results", {'ChallengeKey': challengeKey})
     }
@@ -79,5 +69,4 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   app.isLoggedIn = function(){
     return localStorage.getItem("token") != "" && localStorage.getItem("token") != undefined;
   }
-
 })(document);
--- app/scripts/util.js
+++ app/scripts/util.js
@@ -1,6 +1,6 @@
 var BASE_URL = "/api";
 
-if(location.origin.indexOf("localhost")==-1){
+if (location.origin.indexOf("localhost") == -1) {
   BASE_URL = "https://api.cod.uno";
 }
 
@@ -8,29 +8,14 @@ if(location.origin.indexOf("localhost")==-1){
 function get(ajaxRequest, route){
   ajaxRequest.url = BASE_URL + route;
   ajaxRequest.method = "GET";
-  ajaxRequest.headers = {"Authorization": "Token " + localStorage.getItem("token")};
-  ajaxRequest.generateRequest();
-}
-
-function getBasic(ajaxRequest, route){
-  ajaxRequest.url = BASE_URL + route;
-  ajaxRequest.method = "GET";
-  ajaxRequest.headers = {"Authorization": "Basic " + localStorage.getItem("token")};
-  ajaxRequest.generateRequest();
-}
-
-
-function get(ajaxRequest, route, authType, authValue){
-  ajaxRequest.url = BASE_URL + route;
-  ajaxRequest.method = "GET";
-  ajaxRequest.headers = {"Authorization": authType + " " + authValue};
+  ajaxRequest.headers = {"Authorization": localStorage.getItem("authorization")};
   ajaxRequest.generateRequest();
 }
 
 function post(ajaxRequest, route, body){
   ajaxRequest.url = BASE_URL + route;
   ajaxRequest.method = "POST";
-  ajaxRequest.headers = {"Authorization": "Token " + localStorage.getItem("token")};
+  ajaxRequest.headers = {"Authorization": localStorage.getItem("authorization")};
   ajaxRequest.body = JSON.stringify(body);
   ajaxRequest.generateRequest();
 }
