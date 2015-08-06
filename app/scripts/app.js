5dae3d1ac008fbb5ffa1a7541d6324e89929b1ec
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2015-08-06 15:19:51 +0300

    login: Rewrite of UI, new logic

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,104 +1,100 @@
 (function(document) {
-  'use strict';
+	'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+	// Grab a reference to our auto-binding template
+	// and give it some initial binding values
+	// Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+	var app = document.querySelector('#app');
 
-  app.properties = {
-    challenge: {
-      type: Object,
-      notify: true
-    },
-    isCompany: {
-      type: Boolean,
-      notify: true
-    },
-    isCoder: {
-      type: Boolean,
-      notify: true
-    },
-    isLoggedIn: {
-      type: Boolean,
-      notify: true
-    }
-  };
+	app.properties = {
+		challenge: {
+			type: Object,
+			notify: true
+		},
+		isCompany: {
+			type: Boolean,
+			notify: true
+		},
+		isCoder: {
+			type: Boolean,
+			notify: true
+		},
+		isLoggedIn: {
+			type: Boolean,
+			notify: true
+		}
+	};
 
-  app.login = function(role){
-    localStorage.setItem("role", role);
-    app.refreshMenu();
-    switch(role){
-      case "CODER":
-        page.redirect("/challenge");
-        break;
-      case "COMPANY":
-        page.redirect("/candidates");
-        break;
-      default:
-        page.redirect("/unauthorized");
-    }
-  }
+	app.login = function() {
+		app.refreshMenu();
+		if (!localStorage.accessToken) {
+			return page.redirect("/unauthorized");
+		}
+		if (localStorage.company) {
+			return page.redirect("/candidates");
+		}
+		page.redirect("/challenge");
+	}
 
-  app.refreshMenu = function(){
-    app.isCompany = localStorage.getItem("role") == "COMPANY";
-    app.isCoder = localStorage.getItem("role") == "CODER";
-    app.isLoggedIn = localStorage.getItem("authorization") != "" && localStorage.getItem("authorization") != undefined;
-  }
+	app.refreshMenu = function(){
+		app.isCompany = !!localStorage.company;
+		app.isCoder = !app.isCompany;
+		app.isLoggedIn = localStorage.accessToken;
+	}
 
-  app.displayInstalledToast = function() {
-    console.log('Caching complete.');
-  };
+	app.displayInstalledToast = function() {
+		console.log('Caching complete.');
+	};
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    app.refreshMenu();
-    var challengeKey = localStorage.getItem("challenge");
-    if (challengeKey != "" && challengeKey != undefined) {
-      app.requestData(challengeKey)
-    }
-  });
+	// Listen for template bound event to know when bindings
+	// have resolved and content has been stamped to the page
+	app.addEventListener('dom-change', function() {
+		app.refreshMenu();
+		var challengeKey = localStorage.getItem("challenge");
+		if (challengeKey != "" && challengeKey != undefined) {
+			app.requestData(challengeKey)
+		}
+	});
 
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
+	// See https://github.com/Polymer/polymer/issues/1381
+	window.addEventListener('WebComponentsReady', function() {
+		// imports are loaded and elements have been registered
+	});
 
-  // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onMenuSelect = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
-    if (drawerPanel.narrow) {
-      drawerPanel.closeDrawer();
-    }
-  };
+	// Close drawer after menu item is selected if drawerPanel is narrow
+	app.onMenuSelect = function() {
+		var drawerPanel = document.querySelector('#paperDrawerPanel');
+		if (drawerPanel.narrow) {
+			drawerPanel.closeDrawer();
+		}
+	};
 
-  app.requestData = function(challengeKey){
-    if (challengeKey!="" && challengeKey!= undefined) {
-      localStorage.setItem("challenge", challengeKey);
-      post(app.$.resultRequest, "/results", {'ChallengeKey': challengeKey})
-    }
-  }
+	app.requestData = function(challengeKey){
+		if (challengeKey!="" && challengeKey!= undefined) {
+			localStorage.setItem("challenge", challengeKey);
+			post(app.$.resultRequest, "/results", {'ChallengeKey': challengeKey})
+		}
+	}
 
-  app.onChallengeResponse = function(r){
-    var challenge = r.detail.response;
-    app.challenge = challenge;
+	app.onChallengeResponse = function(r){
+		var challenge = r.detail.response;
+		app.challenge = challenge;
 
-    if(challenge.Tasks.indexOf(localStorage.getItem("currentTask"))==-1){
-      localStorage.removeItem("currentTask");
-      localStorage.removeItem("timer");
-    }
-  }
+		if(challenge.Tasks.indexOf(localStorage.getItem("currentTask"))==-1){
+			localStorage.removeItem("currentTask");
+			localStorage.removeItem("timer");
+		}
+	}
 
-  app.onResultResponse = function(r){
-    var result = r.detail.response
-    localStorage.setItem("result", result.Key);
-    app.result = result
-    get(app.$.challengeRequest, "/challenges/" + localStorage.getItem("challenge"))
-  }
+	app.onResultResponse = function(r){
+		var result = r.detail.response
+		localStorage.setItem("result", result.Key);
+		app.result = result
+		get(app.$.challengeRequest, "/challenges/" + localStorage.getItem("challenge"))
+	}
 
-  app.createChallenge = function(){
-    app.$.taskList.createChallenge();
-  }
+	app.createChallenge = function(){
+		app.$.taskList.createChallenge();
+	}
 
 })(document);
--- app/scripts/util.js
+++ app/scripts/util.js
@@ -1,93 +1,96 @@
-var BASE_URL = "http://localhost:8080";
+var BASE_URL = 'https://api.cod.uno';
 
-if (location.origin.indexOf("localhost") == -1) {
-  BASE_URL = "https://api.cod.uno";
+if (location.origin.indexOf('localhost') !== -1) {
+	BASE_URL = 'http://localhost:8080';
 }
 
-function get(ajaxRequest, route, authorization, responseCallback){
-  ajaxRequest.url = BASE_URL + route;
-  ajaxRequest.method = "GET";
-  if (authorization){
-    ajaxRequest.headers = {"Authorization": authorization};
-  }
-  else{
-    ajaxRequest.headers = {"Authorization": localStorage.getItem("authorization")};
-  }
-  ajaxRequest.generateRequest();
+var accessToken = undefined;
+if (localStorage.accessToken !== undefined) {
+	accessToken = JSON.parse(localStorage.accessToken);
 }
 
-function post(ajaxRequest, route, body){
-  ajaxRequest.url = BASE_URL + route;
-  ajaxRequest.method = "POST";
-  ajaxRequest.headers = {"Authorization": localStorage.getItem("authorization")};
-  ajaxRequest.body = JSON.stringify(body);
-  ajaxRequest.generateRequest();
-}
-function getLanguagesForTags(tags){
-  var langs = [];
-  for(var i=0;i<tags.length;i++){
-    langs.push(getLanguageForTag(tags[i]));
-  }
-  return langs;
+var company = undefined;
+if (localStorage.company !== undefined) {
+	company = JSON.parse(localStorage.company);
 }
 
-function getLanguageForTag(tag){
-  var lang = {tag: tag}
-  switch(tag){
-    case "c":
-      lang.name = "C";
-      break;
-    case "cpp":
-      lang.name = "C++";
-      break;
-    case "java":
-      lang.name = "Java";
-      break;
-    case "py":
-      lang.name = "Python";
-      break;
-    default:
-      lang.name = "Undefined";
-      break;
-  }
-  return lang;
+var user = undefined;
+if (localStorage.user !== undefined) {
+	user = JSON.parse(localStorage.user);
 }
 
+function get(ajaxRequest, route, authorization, responseCallback) {
+	ajaxRequest.url = BASE_URL + route;
+	ajaxRequest.method = 'GET';
+	if (authorization) {
+		ajaxRequest.headers = { 'Authorization': authorization };
+	} else {
+		ajaxRequest.headers = { 'Authorization': 'Token ' + accessToken.Value };
+	}
+	ajaxRequest.generateRequest();
+}
 
-function put(ajaxRequest, route, body){
-  ajaxRequest.url = BASE_URL + route;
-  ajaxRequest.method = "PUT";
-  ajaxRequest.headers = {"Authorization": localStorage.getItem("authorization")};
-  ajaxRequest.body = JSON.stringify(body);
-  ajaxRequest.generateRequest();
+function post(ajaxRequest, route, body) {
+	ajaxRequest.url = BASE_URL + route;
+	ajaxRequest.method = 'POST';
+	ajaxRequest.headers = { 'Authorization': 'Token ' + accessToken.Value };
+	ajaxRequest.body = JSON.stringify(body);
+	ajaxRequest.generateRequest();
+}
+
+function getLanguagesForTags(tags) {
+	var langs = [];
+	for(var i = 0; i < tags.length; i++) {
+		langs.push(getLanguageForTag(tags[i]));
+	}
+	return langs;
+}
+
+function getLanguageForTag(tag) {
+	var lang = { tag: tag }
+	switch (tag) {
+		case 'c':
+			lang.name = 'C';
+			break;
+		case 'cpp':
+			lang.name = 'C++';
+			break;
+		case 'java':
+			lang.name = 'Java';
+			break;
+		case 'py':
+			lang.name = 'Python';
+			break;
+		default:
+			lang.name = 'Undefined';
+			break;
+	}
+	return lang;
+}
+
+function put(ajaxRequest, route, body) {
+	ajaxRequest.url = BASE_URL + route;
+	ajaxRequest.method = 'PUT';
+	ajaxRequest.headers = { 'Authorization': 'Token ' + accessToken};
+	ajaxRequest.body = JSON.stringify(body);
+	ajaxRequest.generateRequest();
 }
 
 (function(){
-  var initialError;
+	var initialError;
 
-  function handleError(error){
-    initialError = error;
-    switch(error.detail.request.xhr.status){
-      case 500: {
-        page.redirect('/500');
-        break;
-      }
-      case 401:{
-        page.redirect('/login');
-        break;
-      }
-      case 404:{
-        //todo: create page for 404
-        page.redirect('/404');
-        break;
-      }
-    }
-  }
+	function handleError(error) {
+		initialError = error;
+		if (error.detail.request.xhr.status == 401) {
+			return page.redirect('/login');
+		}
+		page.redirect('/error');
+	}
 
-  function getLastError(){
-    return initialError;
-  }
+	function getLastError() {
+		return initialError;
+	}
 
-  window.getLastError = getLastError;
-  window.handleError = handleError;
+	window.getLastError = getLastError;
+	window.handleError = handleError;
 })();
