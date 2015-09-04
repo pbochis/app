4777eb491c2f5ca22c5818073128887451266e00
Victor Balan <victor.balan@cod.uno>
2015-09-04 18:00:48 +0300

    Receive results via web sockets
     * Create ws-element

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -10,6 +10,15 @@ var util = {
 
 		return prefix + suffix;
 	},
+	getWSUrl: function(suffix){
+		var prefix = 'ws://api.cod.uno:8090';
+
+		if (location.origin.indexOf('localhost') !== -1) {
+			prefix = 'ws://localhost:8090';
+		}
+
+		return prefix + suffix;
+	},
 	accessToken: function() {
 		if (localStorage.accessToken) {
 			return JSON.parse(localStorage.accessToken);
