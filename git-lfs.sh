8ce1ae1f182c199277b2949fa44c46b032e7e0d3
Lorenz Leutgeb <lorenz.leutgeb@cod.uno>
2016-05-31 11:09:23 +0200

    Fix Git LFS script for Travis CI

--- git-lfs.sh
+++ git-lfs.sh
@@ -7,14 +7,11 @@ VERSION='1.2.0'
 
 echo "Installing ${NAME}-${OS}-${ARCH}-${VERSION}..."
 
-wget -O ${NAME}.tar.gz https://github.com/github/git-lfs/releases/download/v1.2.0/${NAME}-${OS}-${ARCH}-${VERSION}.tar.gz
+wget -O ${NAME}.tar.gz https://github.com/github/git-lfs/releases/download/v${VERSION}/${NAME}-${OS}-${ARCH}-${VERSION}.tar.gz
 tar xvfz ${NAME}.tar.gz
 cd ${NAME}-${VERSION}
 
-echo "Executing install.sh..."
 PREFIX=$HOME ./install.sh
 
-echo "Checking out files from LFS..."
+git lfs fetch
 git lfs checkout
-
-echo "Done!"
